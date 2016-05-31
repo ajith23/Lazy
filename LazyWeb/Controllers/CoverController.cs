20a88ccd89c406f801b8c867c4558875c0d7821c
Ajith <ajithv@axosoft.com>
2016-05-31 13:54:11 -0700

    Initial Version

---
+++ LazyWeb/Controllers/CoverController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LazyWeb.Controllers
+{
+    public class CoverController : Controller
+    {
+        // GET: Cover
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ LazyWeb/scripts/custom/master.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+    var url = $(location).attr('href');
+    $(".nav").find(".active").removeClass("active");
+    $("a").each(function(){
+        if (url.indexOf($(this).attr('href').toLowerCase()) >= 0) {
+            $(this).parent().addClass("active");
+        }
+    });
+    
+});
